604aa3b784b1f9b31a8b43ae03673f6ce17b326f
yeeyee6847 <hoe1686@gmail.com>
2026-06-16 19:03:53 +0800

    UPDATE xiaotutou.py : update send log get channel change to fetch

--- xiaotutou.py
+++ xiaotutou.py
@@ -53,7 +53,7 @@ async def on_ready():
 # ----------------------------------- SEND_LOG START -----------------------------------
 async def send_log(client, message: str):
     print(f"send_log: {message}")
-    channel = client.get_channel(LOG_CHANNEL_ID)
+    channel = await client.fetch_channel(LOG_CHANNEL_ID)
     print(f"send_log: {channel}")
     if channel:
         await channel.send(message)
